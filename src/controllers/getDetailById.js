009c885480dbff58b7631787075afea189ea70c3
iyas89 <iyasasaad@gmail.com>
2023-11-03 13:30:48 -0300

    cambios de controlles

--- src/controllers/getDetailById.js
+++ src/controllers/getDetailById.js
@@ -1,34 +1,43 @@
-const { Events, EventTypes } = require("../db");
+const { Events, EventTypes, Tickets } = require("../db");
 
 const getDetailById = async (id) => {
-  const event = await Events.findAll({
-    where: {
-      id: id,
-    },
-    include: {
-      model: EventTypes,
-      attributes: ["name"],
-    },
-  });
+  const event = await Events.findByPk(id, {
+    include: [
+      {
+        model: EventTypes,
+        attributes: ['name']
+      },
 
-  if (!event || event.length === 0) {
-    throw new Error("This event does not exists");
+      {
+        model: Tickets,
+        attributes: ['id', 'name','stock', 'description', 'price']
+      }
+    ]
+})
+
+if (!event || event.length === 0) {
+    throw new Error("This event does not excist");
+}
+
+  const infoEevnt =  {
+     id: event.id,
+     title: event.title,
+     location: event.location,
+     date: event.date,
+     description: event.description,
+     image: event.image,
+     eventType: event.EventType ? event.EventType.name : null,
+     tickets: event.Tickets.map(ticket => ({
+      id: ticket.id,
+      name: ticket.name,
+      stock:ticket.stock,
+      description: ticket.description,
+      price: ticket.price
+    }))
   }
 
-  const infoEvent = event.map((event) => {
-    return {
-      id: event.id,
-      title: event.title,
-      location: event.location,
-      date: event.date,
-      description: event.description,
-      image: event.image,
-      status: event.status,
-      eventType: event.EventType ? event.EventType.name : null,
-    };
-  });
+  return  infoEevnt
 
-  return infoEvent[0];
 };
 
 module.exports = {
--- src/controllers/getEvents.js
+++ src/controllers/getEvents.js
@@ -1,14 +1,25 @@
-const axios = require("axios");
-const { Events } = require("../db");
-const { Op } = require("sequelize");
-
+const {Events, EventTypes} = require("../db")
+const { Op } = require('sequelize');
 const getEvents = async () => {
-  expiredEvents();
-  const eventsBDD = await Events.findAll({
-    where: { status: "active" },
-  });
+  const events = await Events.findAll({
+    include: {
+        model: EventTypes,
+        attributes: ['name'],
+    }
+});
 
-  return eventsBDD;
+const allEvents = events.map(event => {
+    return {
+        id: event.id,
+        title: event.title,
+        location: event.location,
+        date: event.date,
+        description: event.description,
+        image: event.image,
+        eventType: event.EventType ? event.EventType.name : null   //.toString()
+    }
+})
+  return allEvents;
 };
 
 const getEventsByName = async (name) => {

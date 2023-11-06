f0a6a5d9e130144a68a19d07a7c97f4fd414e3e4
FOteroRimini <facurimini@gmail.com>
2023-11-06 10:17:38 -0300

    MercadoPago

--- src/handlers/createPaymentHandler.js
+++ src/handlers/createPaymentHandler.js
@@ -1,9 +1,10 @@
 const createOrder = require("../controllers/createPayment");
 
 const createPaymentHandler = async (req, res) => {
-    const {id,name, price,description, image,quantity } = req.body;
+    const listCartItems = req.body;
+    console.log(listCartItems);
     try {
-        const newOrder = await createOrder(id,name, price, description, image,quantity)
+        const newOrder = await createOrder(listCartItems);
         res.status(201).json(newOrder);
     } catch (error) {
         res.status(500).json({error: error.message});

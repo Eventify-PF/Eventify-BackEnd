316a4fdc1b3a7d9587c7d09b50b1a0b85fad2c80
FVidalYepz <Frananyelvidalyepez@gmail.com>
2023-11-09 10:24:17 -0400

    newTicket

--- src/models/Tickets.js
+++ src/models/Tickets.js
@@ -1,36 +1,44 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Tickets", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  sequelize.define(
+    "Tickets",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      state: {
+        type: DataTypes.ENUM("active", "inactive"),
+        defaultValue: "active",
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+      },
+      sold: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-    state: {
-      type: DataTypes.ENUM("active", "inactive"),
-      defaultValue: "active",
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-    },
-  },{ timestamps: false });
+    { timestamps: false }
+  );
 };

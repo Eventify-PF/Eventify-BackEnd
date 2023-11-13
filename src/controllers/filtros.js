594937377b3f237bfd62963291a4f151017d3b54
iyas89 <iyasasaad@gmail.com>
2023-11-13 20:04:47 -0300

    filter active 2

--- src/controllers/filtros.js
+++ src/controllers/filtros.js
@@ -2,61 +2,52 @@ const { Events, EventTypes } = require("../db");
 
 const filterByType = async (eventType) => {
   try {
-    
     const eventTypeInstance = await EventTypes.findOne({
-      where: { id: eventType },   
+      where: { id: eventType },
     });
-     
 
-    
-        let filterType = await Events.findAll({
-            where: {
-              EventTypeId: eventTypeInstance.id,
-              status: active
-            },
-        });
-        return filterType;
-        } catch (error) {    
-          throw new Error("Error filtering events:", error);   
-        }
-        };
+    let filterType = await Events.findAll({
+      where: {
+        EventTypeId: eventTypeInstance.id,
+        status: 'active',  
+      },
+    });
+    return filterType;
+  } catch (error) {
+    throw new Error("Error filtering events:", error);
+  }
+};
 
-    
-        const filterByDate = async (date) => {
-            try {
-                let filterDate = await Events.findAll({
-                    where: { date: date, status: active }
-                });
-                return filterDate;
-                
-            } catch (error) {
-                throw new Error("Error filtering events:", error); 
-            }
+const filterByDate = async (date) => {
+  try {
+    let filterDate = await Events.findAll({
+      where: { date: date, status: 'active' },  
+    });
+    return filterDate;
+  } catch (error) {
+    throw new Error("Error filtering events:", error);
+  }
+};
 
-    } 
-    
-    
-    const filterByTypeAndDate = async (eventType, date) => {
-        try {
-            const eventTypeInstance = await EventTypes.findOne({
-                where: { name: eventType },   
-              });
-            let filtercombinado = await Events.findAll({
-                where: {
-                  EventTypeId: eventTypeInstance.id,
-                  date: date,
-                  status: active
-                },
-              });
-              return filtercombinado;
-            
-        } catch (error) {
-            throw new Error("Error filtering events:", error);
-        }
-    }
-    
+const filterByTypeAndDate = async (eventType, date) => {
+  try {
+    const eventTypeInstance = await EventTypes.findOne({
+      where: { id: eventType },
+    });
 
-
+    let filterCombined = await Events.findAll({
+      where: {
+        EventTypeId: eventTypeInstance.id,
+        date: date,
+        status: 'active',  
+      },
+    });
+    return filterCombined;
+  } catch (error) {
+    throw new Error("Error filtering events:", error);
+  }
+};
 
 module.exports = { filterByTypeAndDate, filterByDate, filterByType };
+
 

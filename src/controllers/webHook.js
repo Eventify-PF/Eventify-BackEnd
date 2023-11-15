62b39f8ffe29d3f0acf7ca04e0dd87d47ab03411
Gaston Soliz <gaston.e.soliz@gmail.com>
2023-11-15 02:09:36 -0300

    webhook completo

--- src/controllers/webHook.js
+++ src/controllers/webHook.js
@@ -1,30 +1,44 @@
 const mercadopago = require("mercadopago");
-const {Ticket, Event, TicketUnits, Orders} = require("../db");
-
+const { Ticket, Event, TicketUnits, Orders } = require("../db");
 
 const webHook = async (payment) => {
-    console.log(payment["data.id"])//id de orden de compra
-    if(payment.type == "payment"){
+  console.log(payment["data.id"]); //id de orden de compra
+  if (payment.type == "payment") {
+    const data = await mercadopago.payment.findById(payment["data.id"]);
 
-        const data = await mercadopago.payment.findById(payment["data.id"]);
+    const items = data.body.additional_info.items;
 
-        const items = data.body.additional_info.items;
+    if (data.body.status == "approved") {
+      const dataCreate = await Orders.bulkCreate(
+        items.map((item) => {
+          return {
+            ticketId: item.id,
+            ticketTitle: item.title,
+            quantity: item.quantity,
+            price: item.unit_price,
+            event: item.description,
+            UserId: item.category_id,
+          };
+        })
+      );
 
-        if(data.body.status == "approved"){
-         await Orders.bulkCreate(items.map((item) => {
-            return {
-                ticketId: item.id,
-                ticketTitle: item.title,
-                quantity: item.quantity,
-                price: item.unit_price,
-                event: item.description,
-                UserId: item.category_id,
-            }
-        }));
+      for (const newOrder of dataCreate) {
+        for (let i = 0; i < newOrder.quantity; i++) {
+          const ticketInstance = await TicketUnits.create();
+          await newOrder.addTicketUnits(ticketInstance);
+        }
       }
-
-        return data
     }
-    throw new Error("No se pudo obtener el pago");
-}
+
+    return data;
+  }
+  throw new Error("No se pudo obtener el pago");
+};
+
+// const createUnitTicket = async () => {
+//   try {
+//     const orderInstance = await Orders.findOne({ where: {} });
+//   } catch (error) {}
+// };
+
 module.exports = webHook;
